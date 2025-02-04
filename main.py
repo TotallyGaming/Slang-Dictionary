24810f120c1ee9e6d6f577f5e02e11bbdba475ca
TotallyGaming <ppizzalihamburger@gmail.com>
2025-02-04 21:09:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+# The dictionary itself
 slang_dict = {
-    "SIGMA": "Either a lone ranger or something that's cool",
+    "SIGMA": "Either a lone ranger or something cool",
     "LOL": "Laughing Out Loud (something that is funny)",
-    "GYATT": "Somebodys butt, usually used to point out that it's big",
-    "CRINGE": "Something that is embarrasing or awkward",
+    "GYATT": "Somebody's butt, usually used to point out that it's big",
+    "CRINGE": "Something that is embarrassing or awkward",
     "CAP": "Used to state that something is a lie",
     "CAPPING": "Used to state that someone is lying (see: CAP)",
     "YAPPING": "Talking too much or saying what seems like nonsense to the listener",
@@ -11,14 +12,19 @@ slang_dict = {
     "NGL": "Not Going to Lie (I am being honest)"
 }
 
+# Get input
 word = input("Type slang word to find its meaning:  (in capital letters)")
 found = False
 
+# Search the dictionary
 for slang in slang_dict:
+    # If it matches a key with the input,
     if word in slang and not found:
+        # Print the value
         print(slang_dict[slang])
         found = True
 
+# Pretty self-explainatory
 if not found:
     print("That word isn't in the dictionary.")
 else:
